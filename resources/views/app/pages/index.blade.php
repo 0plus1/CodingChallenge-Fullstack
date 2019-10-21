65af1aaa2196602b68abf6caf0ed6307273dd480
Joey <joybee210@gmail.com>
2019-10-22 00:17:19 +1100

    feat: add front-end side

--- resources/views/app/pages/index.blade.php
+++ resources/views/app/pages/index.blade.php
@@ -1,9 +1,72 @@
 @extends('layouts.app')
 
+@section('style')
+<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+@endsection
+
+@section('script-bottom')
+<script>
+$(async () => {
+    const shelf = 'aqua';
+
+    $('h1').append(`<strong>${shelf}</strong>`);
+    // Books api
+    const bookRequest = axios.create({
+        baseURL: `/api/shelf/${shelf}/read`,
+    });
+
+    const bookRecordMetadataRequest = axios.create({
+        baseURL: '/api/metadata/read',
+    });    
+
+    function checkStatus(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        }
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+
+    function parseJSON(response) {
+        return response.data;
+    }
+
+    const { books } = await bookRequest.get('/')
+                        .then(checkStatus).then(parseJSON);
+
+    books.map(book => {
+        $('.book-container').append(`
+            <li class="book-item" data-book-id=${book.book_id}>
+                <div class="book-name">
+                    Author: <span>${book.name}</span>
+                </div>
+                <div class="book-isbn">
+                    ISBN: <span>${book.isbn}</span>
+                </div>
+                <div class="book-published-time">
+                    Published: <span>${book.created_at}</span>
+                </div>
+            </li>
+        `);
+    });
+
+    $('.book-item').click(async function() {
+        const bookID = $(this).attr('data-book-id');
+        const result = await bookRecordMetadataRequest.get(`/${bookID}`, )
+                        .then(checkStatus).then(parseJSON);
+        
+        console.log(result);
+        alert(JSON.stringify((result)));
+    });    
+
+});
+</script>
+@endsection
+
 @section('content')
-    <div class="row">
-        <div class="col-12">
-            <h1>Welcome!@</h1>
-        </div>
+    <div class="book-section">
+        <h1>Books on shelf: </h1>
+        <ul class="book-container"></ul>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Styles -->
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
+    @yield('style')
 </head>
 <body>
 
@@ -17,6 +19,8 @@
 
 <!-- Scripts -->
 <script src="{{ elixir('js/app.js') }}"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+@yield('script-bottom')
 
 </body>
 </html>
