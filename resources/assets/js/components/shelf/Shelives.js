8e804bc0bb8864c25a37b82cceedcb218aca2d5c
Hugo Wang <hugowangchn@gmail.com>
2022-10-04 01:00:44 +1300

    FE setup

---
+++ resources/assets/js/components/shelf/Shelives.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios"
+
+const Shelives = () => {
+
+  return <div className={'container'}>
+    <label className={'label'}>List</label>
+  </div>;
+};
+
+export default Shelives;
